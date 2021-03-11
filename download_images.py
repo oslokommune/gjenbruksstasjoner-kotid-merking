2dc8422c51a3ae8ac88ed35f498b86a123532c62
Espen Gunnarsen <espen.gunnarsen@byr.oslo.kommune.no>
2021-03-11 14:23:57 +0100

    Update based on PR feedback

--- download_images.py
+++ download_images.py
@@ -9,30 +9,29 @@ Some possible improvements:
 - Move training to AWS (meaning downloading locally will be obsolete).
 """
 
-import sys
 import os
 import re
 from datetime import datetime
-
 from pathlib import Path
+
 import boto3
 
 # HARDCODED PARAMETERS
 BUCKET = "ok-origo-dataplatform-prod"
 PREFIX = "raw/red/REN/station_id_41"
-TARGET_FOLDER = r"./actual images"
+TARGET_FOLDER = "./actual_images"
 
 
 def date_from_key(key: str) -> datetime:
     """
-    Get a S3-key as as a string, return a datetime object generated
-    from the key. Raise an ValueError if any other number than one
+    Get an S3-key as a string, return a datetime object generated
+    from the key. Raise a ValueError if any other number than one
     pattern is found.
     """
 
     matching_datetimes = re.findall(r"\d{8}T\d{6}", key)
     nfindings = len(matching_datetimes)
-    if not nfindings == 1:
+    if nfindings != 1:
         raise ValueError(
             f"1 expected, but found {nfindings} datetime strings within this key:\n{key}"
         )
@@ -57,46 +56,34 @@ def valid_by_datetime(key, date_from=None, date_to=None):
 
     key_dt = date_from_key(key)
 
-    if date_from is not None:
-        if key_dt < date_from:
-            return False
-
-    if date_to is not None:
-        if key_dt > date_to:
-            return False
+    if (date_from and key_dt < date_from) or (date_to and key_dt > date_to):
+        return False
 
     return True
 
 
 def key_downloaded(key, target_folder):
     """
-    Given a S3-key, check if this file has already been downloaded to
+    Given an S3-key, check if this file has already been downloaded to
     the local target_folder.
     """
 
     filename = key.split(r"/")[-1]
     my_file = Path(os.path.join(target_folder, filename))
 
-    if my_file.is_file():
-        return True
-    else:
-        return False
+    return my_file.is_file()
 
 
 def download_file(key, target_folder):
     """
-    Given a S3-key and a local target_folder, download the file there.
+    Given an S3-key and a local target_folder, download the file there.
     """
 
     filename = key.split(r"/")[-1]
     dst = os.path.join(target_folder, filename)
 
     s3 = boto3.client("s3")
-    try:
-        s3.download_file(BUCKET, key, dst)
-    except FileNotFoundError:
-        print(f"ERROR: The target folder '{target_folder}' does not exist.")
-        sys.exit(1)
+    s3.download_file(BUCKET, key, dst)
 
 
 if __name__ == "__main__":
@@ -115,7 +102,7 @@ if __name__ == "__main__":
     new_keys = [k for k in keys if not key_downloaded(k, TARGET_FOLDER)]
 
     # Filter on dates?
-    # Specifiy date_from and date_to. If not given, both defaults to None.
+    # Specify date_from and date_to. If not given, both defaults to None.
     date_from = datetime(2021, 3, 5, 8, 0, 0)
     valid_keys = [key for key in new_keys if valid_by_datetime(key, date_from)]
     print(
@@ -123,11 +110,12 @@ if __name__ == "__main__":
     )
 
     # Download
-    print("Press Ctrl+Z to abort.")
-    counter = 0
-    for vk in valid_keys:
+    print("Press Ctrl+C to abort.")
+    if not os.path.exists(TARGET_FOLDER):
+        print(f"Made folder {TARGET_FOLDER}")
+        os.makedirs(TARGET_FOLDER)
+    for i, vk in enumerate(valid_keys):
         download_file(vk, TARGET_FOLDER)
-        counter += 1
-        print(f"File {counter}/{len(valid_keys)} downloaded: {vk}")
+        print(f"File {i + 1}/{len(valid_keys)} downloaded: {vk}")
 
     print("Done. Now get some coffee and have a blast labelling images!")
